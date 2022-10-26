be2dde09b1729c8dd6e50f5fc9fdad5c4d8368c1
Satvik Kushwaha <satvik213161@gmail.com>
2022-10-26 11:27:07 +0530

    admin basic ui done

--- app/src/main/java/com/example/sportsspace/view/ui/admin/adminhome/AdminHome.java
+++ app/src/main/java/com/example/sportsspace/view/ui/admin/adminhome/AdminHome.java
@@ -1,17 +1,42 @@
 package com.example.sportsspace.view.ui.admin.adminhome;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.sportsspace.R;
 
+
 public class AdminHome extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+
+    @SuppressLint("MissingInflatedId")
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.adminhome);
+
+        drawerLayout = findViewById(R.id.admin_drawer_layout);
+        toolbar = findViewById(R.id.admin_toolbar);
+
+
+        //setting menu listener
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout.isOpen() == true) {
+                    drawerLayout.close();
+                } else {
+                    drawerLayout.open();
+                }
+            }
+        });
     }
 }
